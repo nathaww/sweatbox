eb38ebb673624d53318406c55fb246d9b8f2883e
Nathan Solomon Zegeye <solomonnathan065@gmail.com>
2024-07-09 11:36:58 +0300

    sweatbox

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1C2428",
+        secondary: "#ffffff",
+        tertiary: "#CCA767",
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1024px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
